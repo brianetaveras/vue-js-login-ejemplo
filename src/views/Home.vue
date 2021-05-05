e4b9a0e31082a33a34cc6b37521cd58c0ee11ed7
brianetaveras <taveras574@gmail.com>
2021-05-04 20:32:41 -0400

    Creamos la plantilla del proyecto vue y añadimos formulario de login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="login-page">
+    <div class="card">
+
+
+      <label for="email">Email</label>
+      <input placeholder="example@example.com" v-model="email" class="login-input" type="email" name="email" id="email">
+      
+      <label for="password">Password</label>
+      <input placeholder="Password" v-model="password" class="login-input" type="password" name="password" id="password">
+
+      <button @click="logIn" class="login-btn">Login</button>
+
+      <div v-if="error.length" class="error">Ocurrió un error: {{error}}</div>
+
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+import Vue from 'vue';
+export default Vue.extend(
+  {
+    data(){
+      return {
+        email: '',
+        password: '',
+        error: ''
+      }
+    },
+    methods: {
+      
+      logIn(){
+        if(!this.email || !this.password){
+          this.error = "Debes proveer un email y contraseña";
+          return;
+        }
+        this.error = "";
+        alert('Lo hicimos!')
+      }
+
+    }
+  }
+);
 </script>
+
+<style lang="scss">
+.login-page {
+
+  .card {
+    background: #f1f1f1;
+    border: 1px solid #cdcdcd;
+    margin: 5em auto;
+    max-width: 400px;
+    border-radius: 0.4em;
+    padding: 1em;
+
+    .login-input {
+      display: block;
+      width: 100%;
+      height: 40px;
+      margin-bottom: 10px;
+    }
+
+    .login-btn {
+      width: 100%;
+      height: 40px;
+    }
+
+    .error {
+      text-align: center;
+      margin: 10px 0;
+      color: red;
+    }
+  }
+
+}
+</style>
